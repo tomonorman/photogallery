3237aa8ae132373ed5eeac399255a90ee7c51e2f
Tomo Norman <tomo.l.norman@gmail.com>
2021-01-07 20:58:21 +0900

    modal added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ImageGrid from './comps/ImageGrid';
+import Modal from './comps/Modal';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title/>
       <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      <Modal selectedImg={selectedImg}/>
     </div>
   );
 }
