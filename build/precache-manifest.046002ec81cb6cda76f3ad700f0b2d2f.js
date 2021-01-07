f00d60912e35e9481112882c760b1fd27c42ce5a
Tomo Norman <tomo.l.norman@gmail.com>
2021-01-07 21:59:52 +0900

    prodcution build deployed to firebase

---
+++ build/precache-manifest.046002ec81cb6cda76f3ad700f0b2d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de2741f6b7bf93e20b71cdce5f3247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20b8bf6304a9c8b7eb91",
+    "url": "/static/css/main.a6796318.chunk.css"
+  },
+  {
+    "revision": "b9c06464038dcea82276",
+    "url": "/static/js/2.8a592474.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.8a592474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b8bf6304a9c8b7eb91",
+    "url": "/static/js/main.1fe2f60f.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
